2969a6507d2d12be1e254e30e1af85c05ec1c0a4
jason.chuang@MBP <jason.chuang@ikala.tv>
2019-06-10 15:57:21 +0800

    chore: update broadcast script

--- broadcast.sh
+++ broadcast.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-VIDEO_FILE="your_video.mp4"
-YOUR_RTMP_URL="rtmp://104.199.220.166/myapp"
+FFMPEG_CMD="your_ffmpeg"
+VIDEO_FILE="googleio.mp4"
+YOUR_RTMP_URL="rtmp://104.199.220.166/myhlsdemo"
 
-ffmpeg -re -i $VIDEO_FILE -acodec copy -vcodec copy -f flv $YOUR_RTMP_URL
+$FFMPEG_CMD -re -i $VIDEO_FILE -acodec copy -vcodec copy -f flv $YOUR_RTMP_URL
 
